7aaef6bed6e63df101d8af2637b18141950664f8
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-12-07 21:46:53 -0500

    add response builder

--- src/lib/http.mjs
+++ src/lib/http.mjs
@@ -57,3 +57,73 @@ export async function postRequest (postOptions, body) {
     req.end();
   });
 }
+
+/**
+ * Http Response class to be returned by Lambda functions
+ */
+export class Response {
+  constructor (statusCode, headers, body, isBase64Encoded) {
+    this.statusCode = statusCode;
+    this.headers = headers;
+    this.body = body;
+    this.isBase64Encoded = isBase64Encoded;
+  }
+
+  /**
+   * Return the response
+   * @returns A response object that will be returned from the Lambda function to the API gateway
+   */
+  toJSON () {
+    return {
+      statusCode: this.statusCode,
+      headers: this.headers,
+      body: JSON.stringify(this.body),
+      isBase64Encoded: this.isBase64Encoded
+    };
+  }
+}
+
+/**
+ * Builds a valid Response instance
+ */
+export class ResponseBuilder {
+  constructor () {
+    this.headers = {};
+  }
+
+  setStatusCode (statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  setHeader (key, value) {
+    this.headers[key] = value;
+    return this;
+  }
+
+  setBody (body) {
+    this.body = body;
+    return this;
+  }
+
+  setBase64Encoded (trueOrFalse) {
+    this.isBase64Encoded = trueOrFalse;
+    return this;
+  }
+
+  build () {
+    if (!('statusCode' in this)) {
+      throw new Error('"statusCode" is missing');
+    }
+    if (!('headers' in this)) {
+      throw new Error('"headers" are missing');
+    }
+    if (!('body' in this)) {
+      throw new Error('"body" is missing');
+    }
+    if (!('isBase64Encoded' in this)) {
+      throw new Error('"isBase64Encoded" is missing');
+    }
+    return new Response(this.setStatusCode, this.headers, this.body, this.isBase64Encoded);
+  }
+}
