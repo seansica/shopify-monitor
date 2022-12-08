76f2cf11b431874337403f632365a944baa08259
Sean Sica <23294618+seansica@users.noreply.github.com>
2022-12-07 21:47:50 -0500

    integrate new database class and response builder

--- src/handlers/shopify-handler.mjs
+++ src/handlers/shopify-handler.mjs
@@ -1,31 +1,46 @@
-// Create clients and set shared const values outside of the handler.
-
 import { sendShopifyRequest, processShopifyResponse } from '../lib/shopify.mjs';
-import { writeContentToDatabase } from '../lib/database.mjs';
+import { DynamoTable } from '../lib/database.mjs';
+import { ResponseBuilder } from '../lib/http.mjs';
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.KEEB_STOCK_TABLE;
+// Get environment variables - set by CloudFormation/SAM
 
-// Get the Shopify sites that should be scanned
-const sites = JSON.parse(process.env.SHOPIFY_SITES);
-// const sites = [
-//     "https://shop.norbauer.com/products/norbazaar-rare-and-sundry-topre-aftermarket-housings.js",
-//     "https://shop.norbauer.com/products/the-heavy-grail-hhkb-housing.js",
-//     "https://shop.norbauer.com/products/the-heavy-grail-ghost-of-christmas-future-edition.js"
-// ]
+const inventoryTableName = process.env.INVENTORY_TABLE;
+const configTableName = process.env.CONFIG_TABLE;
+const region = process.env.Region ? process.env.Region : 'us-east-1';
+
+// Initialize database connections
+const configTable = new DynamoTable(region, configTableName, 'name');
+const inventoryTable = new DynamoTable(region, inventoryTableName, 'id');
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ * @param {*} event
+ * @param {*} context
+ * @returns
  */
-export const shopifySynchronizationHandler = async (event, context) => {
+export const shopifySyncHandler = async (event, context) => {
   if ('httpMethod' in event && event.httpMethod !== 'GET') {
     throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
   }
   // All log statements are written to CloudWatch
   console.info('Received event:', event);
 
+  // Prepare the response
+  const responseBuilder = new ResponseBuilder()
+    .setHeader('Content-Type', 'application/json')
+    .setBase64Encoded(false);
+
+  // Retrieve the list of Shopify site URLs to check from the ConfigTable
+  const configItems = await configTable.getAllItems();
+  const sites = [];
+  for (const item of configItems) {
+    if (item.type === 'SITE') {
+      sites.push(item.name);
+    }
+  }
+
   let allItems = [];
-  // FIRST, get the content from the Shopify site(s)
+  // Get the content from the Shopify site(s)
   try {
     for (const site of sites) {
       // Parse the host and path from the full website URL
@@ -37,46 +52,42 @@ export const shopifySynchronizationHandler = async (event, context) => {
       // Parse the list of inventory response
       const listOfStockItems = processShopifyResponse(shopifyResponse, site);
 
-      // Merge the list into the master list - we will return the master list via context.succeed when the function is done processing
+      // Merge the list into the master list which will be returned when the function has completed processing
       allItems = allItems.concat(listOfStockItems);
 
       // Add each item retrieved to the database table
       for (const item of listOfStockItems) {
       /**
+         * Write the content to the database.
          * Each item will be structured like:
          * {
          *      id: number,
          *      title: str,
          *      available: bool,
-         *      quantity: number
+         *      quantity: number,
+         *      site: str
          * }
          */
-        // SECOND write the content to the database
-        await writeContentToDatabase(tableName, item);
+        await inventoryTable.putItem(item);
       }
     }
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(allItems),
-      isBase64Encoded: false
-    };
-    // return context.succeed(res);
+
+    // Return a 200 HTTP response
+    const res = responseBuilder
+      .setStatusCode(200)
+      .setBody(allItems)
+      .build();
+
+    return res.toJSON();
   } catch (err) {
     console.log(err.message);
-    // return context.fail('Failed to handle Shopify content');
-    return {
-      statusCode: 500,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
+    const res = responseBuilder
+      .setStatusCode(500)
+      .setBody({
         title: 'Failed to handle Shopify content',
         error: err.message
-      }),
-      isBase64Encoded: false
-    };
+      })
+      .build();
+    return res.toJSON();
   }
 };
