6246e09d3dc71594dc0c65fcc478e4d3ae9297c2
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-03-19 15:24:43 -0400

    set entrypoint for CDK stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import * as c from 'constructs';
+import { App } from 'aws-cdk-lib';
+import { ShopifyBotStack } from '../lib/shopify-bot-stack';
 
-const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+const app = new App();
+new ShopifyBotStack(app, 'CdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +20,5 @@ new CdkStack(app, 'CdkStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth();
